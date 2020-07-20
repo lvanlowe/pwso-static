6496d88e5dbb52032f3abef12ed0aba7ba2231dc
Van <lvanlowe@inrvisions.com>
2020-07-20 08:49:37 -0400

    add routing

--- pwso-app/src/app/app-routing.module.ts
+++ pwso-app/src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+// const routes: Routes = [];
 
+export const routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+  { path: 'dashboard', component: DashboardComponent, text: 'Dashboard', icon: 'k-i-globe-outline' },
+  // { path: 'deacon', component: DeaconReviewComponent, text: 'Deacon', icon: 'k-i-wrench' },
+  // { path: 'nursery', component: NurseryReviewComponent, text: 'Nursery', icon: 'k-i-preview' },
+  // { path: 'usher', component: UsherReviewComponent, text: 'Usher', icon: 'k-i-gears' },
+  // {
+  //   path: 'products',
+  //   loadChildren: () =>
+  //     import('./products/products.module').then((m) => m.ProductsModule),
+  // },
+  // { path: 'discounts', component: DiscountComponent },
+  // { path: '**', component: NotFoundComponent },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- pwso-app/src/app/app.component.ts
+++ pwso-app/src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { AppState } from './state/app.state';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'pwso-app';
+  items: Array<any> = [];
+
+  constructor(private router: Router, private store: Store<AppState>) {}
+
+  async  ngOnInit() {
+    const routes: any[] = this.router.config;
+
+    routes.forEach(route => {
+      this.items.push({
+          text: route.text,
+          path: route.path ? route.path : '',
+          icon: route.icon
+      });
+    });
+
+    this.items[0].selected = true;
+
+    // this.userInfo = {identityProvider: 'facebook', userDetails: 'Van', userId: 'b6c7c7ed83484c0c9b0c43d0c5302b20', userRoles: ["usher", "deacon", "anonymous", "authenticated"] };
+    // this.store.dispatch(new CreateSuccess(UserInfo, this.userInfo));
+    // this.store.dispatch(new SelectByKey(UserInfo, this.userInfo.userId ));
+
+    // // this.userInfo = await this.getUserInfo();
+
+    // this.checkUser();
+
+    // // this.userInfos = [{identityProvider: 'facebook', userDetails: 'Van', userId: 'b6c7c7ed83484c0c9b0c43d0c5302b20', userRoles: ["usher", "deacon", "anonymous", "authenticated"] }];
+    // this.store.dispatch(new LoadAll(Deacon));
+    // this.store.dispatch(new LoadAllSuccess(UserInfo, this.userInfos ));
+    // this.store.dispatch(new SelectByKey(UserInfo, this.userInfos[0].userId ));
+    // this.store.pipe(select(currentUserInfo)).subscribe(data => (this.testText = data));
+    // this.store.pipe(select(isAdminRole)).subscribe(data => (this.isAdmin = data));
+
+  }
 }
