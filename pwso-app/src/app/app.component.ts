53639fbd16f9751305edbe73911a83c5e9525875
Van <lvanlowe@inrvisions.com>
2020-07-20 20:54:53 -0400

    add auth logic

--- pwso-app/src/app/app.component.ts
+++ pwso-app/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { AppState } from './state/app.state';
+import { UserInfo } from './models/user-info';
+import { CreateSuccess, SelectByKey, Clear } from '@briebug/ngrx-auto-entity';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +12,18 @@ import { AppState } from './state/app.state';
 })
 
 export class AppComponent implements OnInit {
-  title = 'pwso-app';
-  items: Array<any> = [];
+  title = 'Prince William Special Olympics Web App';
+  public expanded = false;
+  public selected = 'Deacons';
+  public items: Array<any> = [];
+  public canLogin = true;
+  public canLogout = false;
+  public isAdmin: boolean;
+  public logButtonText = 'Login';
+  public greeting: string;
+  testText: any;
+  userInfo: UserInfo;
+  userInfos: UserInfo[];
 
   constructor(private router: Router, private store: Store<AppState>) {}
 
@@ -28,13 +40,13 @@ export class AppComponent implements OnInit {
 
     this.items[0].selected = true;
 
-    // this.userInfo = {identityProvider: 'facebook', userDetails: 'Van', userId: 'b6c7c7ed83484c0c9b0c43d0c5302b20', userRoles: ["usher", "deacon", "anonymous", "authenticated"] };
-    // this.store.dispatch(new CreateSuccess(UserInfo, this.userInfo));
-    // this.store.dispatch(new SelectByKey(UserInfo, this.userInfo.userId ));
+    this.userInfo = {identityProvider: 'facebook', userDetails: 'Van', userId: 'b6c7c7ed83484c0c9b0c43d0c5302b20', userRoles: ["usher", "deacon", "anonymous", "authenticated"] };
+    this.store.dispatch(new CreateSuccess(UserInfo, this.userInfo));
+    this.store.dispatch(new SelectByKey(UserInfo, this.userInfo.userId ));
 
     // // this.userInfo = await this.getUserInfo();
 
-    // this.checkUser();
+    this.checkUser();
 
     // // this.userInfos = [{identityProvider: 'facebook', userDetails: 'Van', userId: 'b6c7c7ed83484c0c9b0c43d0c5302b20', userRoles: ["usher", "deacon", "anonymous", "authenticated"] }];
     // this.store.dispatch(new LoadAll(Deacon));
@@ -45,8 +57,63 @@ export class AppComponent implements OnInit {
 
   }
 
-  onButtonClick() {
-    this.title = 'Hello from Kendo UI!';
+  // onButtonClick() {
+  //   this.title = 'Hello from Kendo UI!';
+  // }
+
+  async getUserInfo() {
+    try {
+      const response = await fetch('/.auth/me');
+      const payload = await response.json();
+      const { clientPrincipal } = payload;
+      this.userInfo = clientPrincipal;
+      this.store.dispatch(new CreateSuccess(UserInfo, this.userInfo));
+      this.store.dispatch(new SelectByKey(UserInfo, this.userInfo.userId ));
+      return clientPrincipal;
+    } catch (error) {
+      console.error('No profile could be found');
+      return undefined;
+    }
+  }
+
+  onLogon() {
+    if (this.canLogin) {
+      this.canLogin = false;
+      this.canLogout = false;
+    } else {
+      this.canLogin = true;
+      this.canLogout = false;
+      this.logButtonText = 'Login';
+      this.greeting = '';
+      this.store.dispatch(new Clear(UserInfo));
+    }
+
+  }
+
+  checkUser() {
+    if (this.userInfo) {
+      this.canLogin = false;
+      this.canLogout = true;
+      this.logButtonText = 'Logout';
+      this.greeting = 'Hi ' + this.userInfo.userDetails;
+    } else {
+      this.canLogin = true;
+      this.canLogout = false;
+      this.greeting = '';
+    }
+  }
+
+  goAuth(provider: string) {
+    // const { pathname } = window.location;
+    // const redirect = `post_login_redirect_uri=${pathname}`;
+    // const url = `/.auth/login/${provider}?${redirect}`;
+    // window.location.href = url;
+
+    this.userInfo = {identityProvider: 'facebook', userDetails: 'Van', userId: 'b6c7c7ed83484c0c9b0c43d0c5302b20', userRoles: ["usher", "deacon", "anonymous", "authenticated"] };
+    this.store.dispatch(new CreateSuccess(UserInfo, this.userInfo));
+    this.store.dispatch(new SelectByKey(UserInfo, this.userInfo.userId ));
+
+    this.checkUser();
   }
 
 }
