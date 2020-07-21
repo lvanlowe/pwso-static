3a476c584d2ac30ce1e4840c4088c51d97dbb059
Van <lvanlowe@inrvisions.com>
2020-07-21 09:46:01 -0400

    add registration

--- pwso-app/src/app/app-routing.module.ts
+++ pwso-app/src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { RegistrationComponent } from './sports/registration/registration.component';
 
 // const routes: Routes = [];
 
 export const routes = [
   { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
   { path: 'dashboard', component: DashboardComponent, text: 'Dashboard', icon: 'k-i-globe-outline' },
-  // { path: 'deacon', component: DeaconReviewComponent, text: 'Deacon', icon: 'k-i-wrench' },
+  { path: 'registration', component: RegistrationComponent, text: 'Registration', icon: 'k-i-wrench' },
   // { path: 'nursery', component: NurseryReviewComponent, text: 'Nursery', icon: 'k-i-preview' },
   // { path: 'usher', component: UsherReviewComponent, text: 'Usher', icon: 'k-i-gears' },
   // {
