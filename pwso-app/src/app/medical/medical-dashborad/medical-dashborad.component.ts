f42859d583829b5443a3606ba2c69412d8e31586
Van <lvanlowe@inrvisions.com>
2021-03-24 09:41:58 -0400

    Add display functionality

--- pwso-app/src/app/medical/medical-dashborad/medical-dashborad.component.ts
+++ pwso-app/src/app/medical/medical-dashborad/medical-dashborad.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-medical-dashborad',
@@ -9,9 +9,36 @@ import { FormGroup } from '@angular/forms';
 export class MedicalDashboradComponent implements OnInit {
 
   medicalDashboardForm: FormGroup;
-  constructor() { }
+  displayList: boolean;
+  canAdd: boolean;
+  canCancel: boolean;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.buildMedicalForm(this.formBuilder);
+    this.medicalDashboardForm.markAsPristine();
+    this.displayList = true
+    this.canAdd = true;
   }
 
+  buildMedicalForm(formBuilder: FormBuilder) {
+    this.medicalDashboardForm = formBuilder.group(
+      {
+
+      }
+    );
+  }
+
+  clickAdd() {
+    this.canCancel = true;
+    this.displayList = false;
+    this.canAdd = false;
+    // this.diaconateid = null;
+  }
+
+  clickCancel() {
+    this.canAdd = true;
+    this.displayList = true;
+  }
 }
