07126ed2870b0bbdbd84ba39b02240f3b9041e83
Van <lvanlowe@inrvisions.com>
2021-04-08 11:13:55 -0400

    added function to get all Athletes

---
+++ api/GetAthletesFunc.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+using pwsoProcesses.Models;
+
+namespace api
+{
+    public static class GetAthletesFunc
+    {
+        [FunctionName("GetAthletesFunc")]
+        [OpenApiOperation(operationId: "Run", tags: new[] { "name" })]
+        [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
+        [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [CosmosDB(
+                databaseName: "pwso",
+                collectionName: "athlete",
+                SqlQuery = "SELECT * FROM athlete",
+                ConnectionStringSetting = "CosmosDBConnection")]IEnumerable<AthleteDb> athleteDocuments,
+
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            return new OkObjectResult(athleteDocuments);
+        }
+    }
+}
+
