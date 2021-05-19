01a0ebd1b1ab6cc8c3f516c8cd5bee590eab61d8
Van <lvanlowe@inrvisions.com>
2021-05-19 10:05:19 -0400

    add search results model

---
+++ pwsoProcesses/Models/AthleteSearchResultsDb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pwsoProcesses.Models
+{
+    public class AthleteSearchResultsDb
+    {
+        public string id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string gender { get; set; }
+        public DateTime medicalExpirationDate { get; set; }
+        public string medicalFormEndDate => medicalExpirationDate.ToShortDateString();
+
+    }
+}
